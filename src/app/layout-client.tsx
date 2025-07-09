ff3d4df52d2de31de0161982d12f705f6cbfe458
Jeanluca <jeanlucafp@gmail.com>
2025-07-09 10:24:46 -0300

    Remove ThemeProvider and related theme toggle functionality from Header component

--- src/app/layout-client.tsx
+++ src/app/layout-client.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { SettingsConsumer } from "./context/settings/settings-consumer";
 import { SettingsProvider } from "./context/settings/settings-provider";
-import { ThemeProvider, Theme } from "./ThemeProvider";
 import Header from "./Header";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
@@ -12,12 +11,12 @@ export default function Layout({ children }: { children: React.ReactNode }) {
                     if (!settings.isInitialized) {
                         return <div>Loading...</div>
                     }
-                    console.log("settings", settings);
+
                     return (
-                        <ThemeProvider theme={settings.theme as Theme}>
+                        <>
                             <Header />
                             {children}
-                        </ThemeProvider>
+                        </>
                     )
                 }}
             </SettingsConsumer>
